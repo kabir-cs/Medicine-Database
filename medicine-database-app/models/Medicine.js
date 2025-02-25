713299b4a6397d0c7a2ebe63c189b848fff3ef64
kabir-cs <kabirs@usf.edu>
2025-02-25 00:00:48 -0500

    new

--- medicine-database-app/models/Medicine.js
+++ medicine-database-app/models/Medicine.js
@@ -2,7 +2,8 @@ import mongoose from 'mongoose';
 
 const MedicineSchema = new mongoose.Schema({
   name: String,
+  name: someting
   description: String
-});
+});commit
 
 export default mongoose.models.Medicine || mongoose.model('Medicine', MedicineSchema);
